5870da549a67558294ea1dc96519d13135fc00d7
Nilton Miguel <niltonguimaraessouza@gmail.com>
2025-06-02 13:51:07 -0300

    adicionei suporte a stereo

--- main.c
+++ main.c
@@ -12,12 +12,13 @@
 #include <limits.h>
 
 #define SAMPLE_WINDOW_BUFFER_SIZE 1024
-#define TIME_LENGTH_SECONDS 5
+#define TIME_LENGTH_SECONDS 4
 
 long int i;
 
 int main(){
 
+    // leitura do audio -----------------------------------------------------------------------------------------------
         // abrir o arquivo de audio e obter metadados
         SF_INFO info_wav_entrada;
         info_wav_entrada.format = 0;
@@ -31,13 +32,19 @@ int main(){
     long int BUFFER_SIZE = signal_length * info_wav_entrada.channels;
     float buffer[BUFFER_SIZE];
     sf_count_t FRAMES = signal_length;
-
     sf_readf_float (wav_entrada, buffer, FRAMES);
 
-    // o sinal recebe apenas um dos canais de cada frame
-    float sinal[signal_length];
-    for (i = 0; i < BUFFER_SIZE/info_wav_entrada.channels; i++) sinal[i] = buffer[info_wav_entrada.channels*i];
+    // cada sinal a ser processado deve receber apenas um canal do buffer 
+    // esquerdo : canal 0
+    // direito  : canal 1
+    float esquerdo[signal_length];
+    float direito[signal_length];
 
+    for (i = 0; i < signal_length; i++)
+    {
+        esquerdo[i]     = buffer[info_wav_entrada.channels * i];
+        direito[i]      = buffer[info_wav_entrada.channels * i + 1];
+    }
     sf_close  (wav_entrada);
     //------------------------------------------------------------------------------------------------------------------
 
@@ -46,11 +53,21 @@ int main(){
     long int DURATION = (long int) (0.5 * sampling_rate);
     float DECAY = 0.3;
 
-    float output[signal_length];
-    float residual[DURATION];
-    for(i=0; i<DURATION; i++) residual[i] = 0;
+    // cada canal precisa ser processado separadamente
+    float output_esquerdo[signal_length];
+    float output_direito[signal_length];
 
-    echo(sinal, output, residual, signal_length, DURATION, DECAY);
+    float residual_esquerdo[DURATION];
+    float residual_direito[DURATION];
+
+    for(i=0; i<DURATION; i++) 
+    {
+        residual_esquerdo[i] = 0;
+        residual_direito[i] = 0;
+    }
+
+    echo(esquerdo, output_esquerdo, residual_esquerdo, signal_length, DURATION, DECAY);
+    echo(direito, output_direito, residual_direito, signal_length, DURATION, DECAY);
 
     // audio output ----------------------------------------------------------------------------------------------------
 
@@ -60,11 +77,11 @@ int main(){
             SF_INFO info_wav_output;
 
             info_wav_input.samplerate = sampling_rate;
-            info_wav_input.channels = 1;
+            info_wav_input.channels = 2;
             info_wav_input.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
 
             info_wav_output.samplerate = sampling_rate;
-            info_wav_output.channels = 1;
+            info_wav_output.channels = 2;
             info_wav_output.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
 
         // abrir os arquivos em modo de escrita
@@ -72,8 +89,23 @@ int main(){
         SNDFILE * wav_output =  sf_open("out.wav", SFM_WRITE, &info_wav_output);
 
         // escrever os dados e liberar os arquivos
-        sf_write_float(wav_input, sinal, signal_length);
-        sf_write_float(wav_output, output, signal_length);
+
+        float input_stereo[signal_length*info_wav_entrada.channels];
+        for(i=0; i<signal_length; i++)
+        {
+            input_stereo[info_wav_entrada.channels*i] =   esquerdo[i];
+            input_stereo[info_wav_entrada.channels*i+1] = direito[i];
+        }
+        
+        float output_stereo[signal_length*info_wav_entrada.channels];
+        for(i=0; i<signal_length; i++)
+        {
+            output_stereo[info_wav_entrada.channels*i] =   output_esquerdo[i];
+            output_stereo[info_wav_entrada.channels*i+1] = output_direito[i];
+        }
+        
+        sf_writef_float(wav_input, input_stereo, signal_length);
+        sf_writef_float(wav_output, output_stereo, signal_length);
 
         sf_close(wav_input);
         sf_close(wav_output);
@@ -91,14 +123,14 @@ int main(){
         printf("\n");
         for(i=0; i<signal_length; i++){
 
-            fprintf(fp, "%ld %f\n", i, sinal[i]);
+            fprintf(fp, "%ld %f %f\n", i, buffer[i], input_stereo[i]);
         }
         fprintf(gnupipe, "%s\n", GnuCommands[0]); // invocar o gnuplot
 
         fclose(fp);
         fclose(gnupipe);
         */
-
+               
     //------------------------------------------------------------------------------------------------------------------
 
     return 0;
